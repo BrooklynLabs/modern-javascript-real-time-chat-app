9e737e26adfe3072d8eca76befc4aa0f7265474e
Ryan Chimento <ryanchimento@gmail.com>
2016-06-18 14:52:13 -0400

    define example middleware and setup Jade template files

--- full-application/01-express/server.js
+++ full-application/01-express/server.js
@@ -6,10 +6,38 @@ var http = require("http"),
 	express = require("express");
 
 const app = express();
+app.set("view engine", "jade");
+
+//Example of creating middleware (a function that wraps something in a pipeline)
+//browser -> node -> express -> Middleware1 -> Middleware 2 -> handler
+//        <-      <-         <-             <-              <- 
+
+//.use() function invokes middleware
+app.use((request, response, next) => {
+	console.log("--- In middleware 1");
+	next();
+	console.log("--- Out of middleware 1");
+});
+
+// Using built-in middleware to serve out static files -> starts a synchronous request
+app.use(express.static("./public"));
+
+app.use((request, response, next) => {
+	console.log("--- In middleware 2");
+	next(); //.use() will not work if next() is not executed --> needs next to move the pipeline along
+	console.log("--- Out of middleware 2");
+});
 
 //This a route, establishes config for express. Express not work out of the box
 app.get("/", (request, response) => {
 	response.end("Hello, World!!");
+	console.log("In handler");
+});
+
+app.get("/home", (request, response) => {
+	// Renders out the view passed in (index) to the view engine. Jade default will look for a "views" folder, so naming is important
+	// values on the right equate to those in the jade file
+	response.render("index", {title: "TITLE"});
 });
 
 const server = new http.Server(app);
